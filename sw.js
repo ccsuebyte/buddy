1d2431b76d92fd98969cc8303c8171a99c7c0148
ccsuebyte <ccasuarez@gmail.com>
2026-05-19 13:55:55 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,39 +1,21 @@
-const CACHE_NAME = "buddy-v2";
+const CACHE_NAME = "buddy-cache-v1"
 
 const urlsToCache = [
   "./",
-  "./index.html"
-];
+  "./index.html",
+  "./manifest.json"
+]
 
 self.addEventListener("install", event => {
-
-  self.skipWaiting();
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
-  );
-
-});
-
-self.addEventListener("activate", event => {
-
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
-
-});
+  )
+})
 
 self.addEventListener("fetch", event => {
-
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-  );
-
-});
+  )
+})
