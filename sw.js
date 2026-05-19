37d4dfdb28436fffbf65d6a51ffc793fac31a465
ccsuebyte <ccasuarez@gmail.com>
2026-05-19 15:12:35 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,26 @@
-const CACHE_NAME = "buddy-cache-v1"
+const CACHE = 'buddy-v1';
+const ASSETS = [
+  './buddy.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Geist:wght@300;400;500;600&display=swap'
+];
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-]
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  )
-})
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  )
-})
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./buddy.html')))
+  );
+});
